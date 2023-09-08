98d9d8e8699bbb89bf7a83077c568f93cdf0c0f5
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2023-09-08 00:54:26 -0500

    Code to turn the tank

--- tank.js
+++ tank.js
@@ -3,6 +3,9 @@ TANK_WIDTH = 20;
 CANNON_HEIGHT = 5.5;
 CANNON_WIDTH = 5.5;
 
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
 class Tank {
   constructor(x, y, color) {
     this.x = x;
@@ -10,8 +13,18 @@ class Tank {
     this.color = color || 'red';
   }
 
-  draw(ctx) {
+  draw() {
     ctx.fillStyle = this.color;
+
+    // Turn the tank. TODO: REFACTOR
+    let [centerX, centerY] = [
+      this.x + TANK_WIDTH / 2,
+      this.y + TANK_HEIGHT / 2,
+    ];
+    ctx.translate(centerX, centerY);
+    ctx.rotate(Math.PI / 2);
+    ctx.translate(-centerX, -centerY);
+
     // tank body
     ctx.fillRect(this.x, this.y, TANK_WIDTH, TANK_HEIGHT);
 
@@ -25,6 +38,5 @@ class Tank {
   }
 }
 
-ctx = document.getElementById('canvas').getContext('2d');
 tank = new Tank(10, 10);
-tank.draw(ctx);
+tank.draw();
