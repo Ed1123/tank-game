a601a7b2457044fb320ae86c222d6bd912f4ea5d
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2023-09-08 19:41:53 -0500

    Add random tank colors and directions!

--- tank.js
+++ tank.js
@@ -6,23 +6,41 @@ CANNON_WIDTH = 5.5;
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+const CANVAS_WIDTH = canvas.width;
+const CANVAS_HEIGHT = canvas.height;
+
 class Tank {
-  constructor(x, y, color) {
+  constructor(x, y, direction, color) {
     this.x = x;
     this.y = y;
     this.color = color || 'red';
+    this.direction = direction || 'up';
   }
 
   draw() {
     ctx.fillStyle = this.color;
 
-    // Turn the tank. TODO: REFACTOR
+    // Turn the tank
     let [centerX, centerY] = [
       this.x + TANK_WIDTH / 2,
       this.y + TANK_HEIGHT / 2,
     ];
     ctx.translate(centerX, centerY);
-    ctx.rotate(Math.PI / 2);
+    switch (this.direction) {
+      case 'up':
+        break;
+      case 'right':
+        ctx.rotate(Math.PI / 2);
+        break;
+      case 'left':
+        ctx.rotate(-Math.PI / 2);
+        break;
+      case 'down':
+        ctx.rotate((3 * Math.PI) / 2);
+        break;
+      default:
+        console.error(`${this.direction} is incorrect.`);
+    }
     ctx.translate(-centerX, -centerY);
 
     // tank body
@@ -35,8 +53,30 @@ class Tank {
       CANNON_WIDTH,
       CANNON_HEIGHT
     );
+
+    // reset context for next drawings
+    ctx.resetTransform();
   }
 }
 
-tank = new Tank(10, 10);
-tank.draw();
+function rand(from, to) {
+  return Math.random() * (to - from) + from;
+}
+
+function randInt(from, to) {
+  return Math.floor(rand(from, to));
+}
+
+function randColor() {
+  return `rgb(${randInt(0, 256)}, ${randInt(0, 256)}, ${randInt(0, 256)})`;
+}
+
+for (let i = 0; i < 5; i++) {
+  tank = new Tank(
+    rand(TANK_WIDTH / 2, CANVAS_WIDTH - TANK_WIDTH / 2),
+    rand(TANK_HEIGHT / 2, CANVAS_HEIGHT - TANK_HEIGHT / 2),
+    ['up', 'right', 'left', 'down'][randInt(0, 4)],
+    randColor()
+  );
+  tank.draw();
+}
