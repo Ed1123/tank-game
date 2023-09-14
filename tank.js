61d3b949970e4f0ff7f0b789105b7086be74d1b8
EdXPS <44955050+Ed1123@users.noreply.github.com>
2023-09-13 23:22:40 -0500

    Move tank with arrows!

--- tank.js
+++ tank.js
@@ -61,8 +61,9 @@ class Tank {
     ctx.resetTransform();
   }
 
-  update() {
-    switch (this.direction) {
+  move(direction) {
+    this.direction = direction;
+    switch (direction) {
       case 'up':
         this.y -= this.speed;
         break;
@@ -76,7 +77,7 @@ class Tank {
         this.y += this.speed;
         break;
       default:
-        console.error(`${this.direction} is incorrect.`);
+        console.error(`${direction} is incorrect.`);
     }
   }
 }
@@ -95,22 +96,73 @@ function generateRandTanks(n) {
 }
 
 const randTanks = generateRandTanks(4);
-// const test_tank = new Tank(
-//   CANVAS_WIDTH / 2,
-//   CANVAS_HEIGHT - (3 / 2) * TANK_HEIGHT,
-//   'down',
-//   'white'
-// );
-// tanks.push(test_tank);
+const tank = new Tank(
+  CANVAS_WIDTH / 2,
+  CANVAS_HEIGHT - (3 / 2) * TANK_HEIGHT,
+  'down',
+  'white'
+);
+
+let rightPressed = false;
+let leftPressed = false;
+let upPressed = false;
+let downPressed = false;
+
+function handleKeyDown(event) {
+  switch (event.code) {
+    case 'ArrowRight':
+      rightPressed = true;
+      break;
+    case 'ArrowLeft':
+      leftPressed = true;
+      break;
+    case 'ArrowUp':
+      upPressed = true;
+      break;
+    case 'ArrowDown':
+      downPressed = true;
+      break;
+  }
+}
+
+function handleKeyUp(event) {
+  switch (event.code) {
+    case 'ArrowRight':
+      rightPressed = false;
+      break;
+    case 'ArrowLeft':
+      leftPressed = false;
+      break;
+    case 'ArrowUp':
+      upPressed = false;
+      break;
+    case 'ArrowDown':
+      downPressed = false;
+      break;
+  }
+}
+
+document.addEventListener('keydown', handleKeyDown);
+document.addEventListener('keyup', handleKeyUp);
 
 function game() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
   ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
+  tank.draw();
   for (const tank of randTanks) {
-    tank.update();
+    tank.move();
     tank.draw();
   }
+  if (rightPressed) {
+    tank.move('right');
+  } else if (leftPressed) {
+    tank.move('left');
+  } else if (upPressed) {
+    tank.move('up');
+  } else if (downPressed) {
+    tank.move('down');
+  }
+
   requestAnimationFrame(game);
 }
 game();
